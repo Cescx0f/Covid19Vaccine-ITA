a47b5bfaab21635a98494e62ad747c0fd229544d
francesco.scarpa <francesco.scarpa@jazznetworks.com>
2020-12-28 16:53:35 +0100

    ADD: basic functionality

--- business/utils.ts
+++ business/utils.ts
@@ -1,11 +1,20 @@
 import { population } from './demographic';
+import { add } from 'date-fns'
 
 const VACCINE_PER_WEEK = 480000 
 const VACCINE_PER_DAY = VACCINE_PER_WEEK/7;
 
 
+const ALREADY_VACCINATED = 0;
+
 
 const START_VACCINE_DATE_MILLIS = new Date('28 Dec 2020').getTime();
+
+export function computeTotPop() {
+    let tot = 0;
+    population.forEach((pop) => {tot += pop} );
+    return tot;
+}
 
 export function isValidAge(age:number): boolean {
     return age != null && age >= 0;
@@ -17,21 +26,42 @@ export function popAheadOfAge(age: number): number {
     }
 
     let popAhead = 0;
+    // starting from older population
     for (let iterAge = population.length -1; iterAge >= 0; iterAge--) {
         if(iterAge === age -1) {
             break;
         }
 
-        // console.log(population[iterAge])
+        console.log('add pop with age, ', iterAge +1)
+
         const currentPop = population[iterAge];
         popAhead += currentPop;
     }
 
-    return popAhead;
+    return popAhead - ALREADY_VACCINATED;
 
 }
 
-// export function startVaccineDate(age: number) {
-//     const popAhead = popAheadOfAge(age);
+export interface VaccineExpected {
+    startDate: number;
+    endDate: number;
+}
+
+export function computeVaccineDates(age: number): VaccineExpected {
+    const popAhead = popAheadOfAge(age);
+
+    const weeksAheadOfUser = Math.trunc(popAhead/VACCINE_PER_WEEK);
+
+    const weeksForUserAge = Math.max(Math.trunc(population[age]/VACCINE_PER_WEEK), 1)
+    console.log('weeksForUserAge',  weeksForUserAge, age, population[age],);
+
+    const startDate = add(START_VACCINE_DATE_MILLIS, {weeks: weeksAheadOfUser})
+
+    const endDate = add(startDate, {weeks: weeksForUserAge});
+
+    return {
+        startDate: startDate.getTime(),
+        endDate: endDate.getTime(),
+    }
     
-// }
+}
--- components/result.vue
+++ components/result.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-      POP AHEAD:  {{popAhead}}
+    <div>
+        Ci sono {{popAhead}} persone davanti a te ({{popAheadPerc}}% della popolazione totale)
+    </div>
+    <div>
+        Ti vaccinerai fra il {{startDate}} e il {{endDate}}
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import { popAheadOfAge, isValidAge } from '~/business/utils';
+import { popAheadOfAge, isValidAge, computeVaccineDates, computeTotPop } from '~/business/utils';
 
 export default Vue.extend({
     props: {
@@ -30,9 +36,30 @@ export default Vue.extend({
 
             return this.age;
         },
+
+        totPop: function(): number {
+            return computeTotPop();
+        },
         
         popAhead: function(): number {
             return popAheadOfAge(this.computedAge);
+        },
+
+        popAheadPerc: function(): number {
+            const rawPerc = (100 * this.popAhead) / this.totPop;
+            return Math.round(rawPerc * 100) / 100
+        },
+
+        vaccineDates: function(): VaccineExpected {
+            return computeVaccineDates(this.computedAge);
+        },
+
+        startDate: function(): string {
+            return new Date(this.vaccineDates.startDate).toLocaleDateString()
+        },
+
+        endDate: function(): string {
+            return new Date(this.vaccineDates.endDate).toLocaleDateString()
         }
     }
 
